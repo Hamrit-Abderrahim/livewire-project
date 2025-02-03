d7b737b3ee0b5efc74e525b3cd6f0c9c85afc001
Hamrit-Abderrahim <escobarp248@gmail.com>
2025-02-03 05:12:42 +0100

    front integration

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
+
+//!------ front Route -----------------------
+
+Route::prefix('/')->name('front.')->group(function () {
+    //?------------ index page --------------------
+     Route::view('', 'front.index')->name('index');
+    //?------------ about page --------------------
+     Route::view('about', 'front.about')->name('about');
+    //?------------ about page --------------------
+     Route::view('contact', 'front.contact')->name('contact');
+    //?------------ project page --------------------
+     Route::view('project', 'front.projects')->name('projects');
+    //?------------ services page --------------------
+     Route::view('services', 'front.services')->name('services');
+    //?------------ team page --------------------
+     Route::view('team', 'front.team')->name('team');
+    //?------------ testimonial page --------------------
+     Route::view('testimonial', 'front.testimonial')->name('testimonial');
+
+});
